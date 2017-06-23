57f54440d2e9e6e8d76de621e02c097c2fd66804
linguohao <guohao_lin@ziguangcn.com>
2017-06-23 09:23:27 +0800

    add cnblogs

---
+++ tutorial/spiders/cnblogs.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+from tutorial.items import blogItem
+
+
+add = 0
+class CnblogsSpider(CrawlSpider):
+    name = "cnblogs"
+    allowed_domains = ["cnblogs.com"]
+    start_urls = [
+        'https://www.cnblogs.com/lgh344902118',
+    ]
+
+    rules = [
+        Rule(SgmlLinkExtractor(allow=('page=\d+', ), ), callback='parse_item', follow=True),
+    ]
+
+    def parse_start_url(self, response):
+        return self.parse_item(response)
+
+    def parse_item(self, response):
+        global add
+
+        print add
+
+        sel = Selector(response)
+        items = []
+
+        titles = sel.xpath('//*[@id="mainContent"]//*[@class="postTitle"]/a/text()').extract()
+        descriptions = sel.xpath('//*[@id="mainContent"]//*[@class="postCon"]/div/text()').extract()
+        urls = sel.xpath('//*[@id="mainContent"]//*[@class="postTitle"]/a/@href').extract()
+        for i in range(len(titles)):
+            item = blogItem()
+            item['head_title'] = titles[i]
+            item['description'] = descriptions[i]
+            item['url'] = urls[i]
+            items.append(item)
+            add += 1
+        return items
